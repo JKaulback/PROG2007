778ad56567bf9ce6d6a5f9266fa9987e4122e95a
JKaulback <jtkaulback@gmail.com>
2025-01-21 12:20:38 -0400

    Finished exercise: Control Flow Part II

--- Exercises/Control Flow Part II/bouncy-ball.c
+++ Exercises/Control Flow Part II/bouncy-ball.c
@@ -3,17 +3,24 @@
 int main() {
     // Set up variables
     int numberBounces;
-    float initialHeight;
+    float currentHeight;
     float bouncinessIndex;
+    float totalDistance;
     // Get user input
     printf("Enter the initial height of the ball: ");
-    scanf("%f", &initialHeight);
+    scanf("%f", &currentHeight);
     printf("Enter the number of bounces: ");
     scanf("%d", &numberBounces);
     printf("Enter the bounciness index: ");
     scanf("%f", &bouncinessIndex);
     // Evaluate total vertical distance of ball
-    
+    totalDistance = currentHeight;
+    for (int i = 0; i < numberBounces; i++) {
+        currentHeight *= bouncinessIndex;
+        totalDistance += currentHeight * 2;
+    }
+    // Output the total vertical distance of the ball
+    printf("The ball travelled %.2f ft.\n", totalDistance);
     // Exit program
     return 0;
 }
