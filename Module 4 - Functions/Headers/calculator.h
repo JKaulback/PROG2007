695d35e43de825f7fec53e47ebcc78e9e63feb30
JKaulback <jtkaulback@gmail.com>
2025-02-03 17:25:41 -0400

    Learned about header files

---
+++ Module 4 - Functions/Headers/calculator.h
@@ -0,0 +1,9 @@
+// --Variables
+
+// --Functions
+// This both declares and defines the add() function in this header file
+// int add(int n1, int n2) {
+//     return n1 + n2;
+// }
+// It is better practice to only declare the function in the header file
+int add(int, int);
---
+++ Module 4 - Functions/Headers/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "calculator.h"
+
+int main() {
+
+    int result = add(10, 100);
+    printf("Result: %d\n", result);
+
+    return 0;
+}
