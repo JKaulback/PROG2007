1e228cc84a8fa79c3d0dad2ced466b070fc21e15
JKaulback <jtkaulback@gmail.com>
2025-02-18 08:33:35 -0400

    Created submission folder

---
+++ Assignments/Assignment 1/Submission/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main() {
+    greetUser();
+    while (1) {
+        printMenu();
+        int userChoice;
+        scanf("%d", &userChoice);
+        getchar();
+        if (userChoice == 1) {
+            int num1 = 0, num2 = 0;
+            printf("Welcome to the number adder\n");
+            add( num1, num2 );
+        } else if (userChoice == 2) {
+            char password[100];
+            printf("Make sure your password has no spaces in it...\n");
+            printf("Enter the password to encrypt:");
+            scanf("%s", password);
+            while (getchar() != '\n');
+
+            encrypt(password, sizeof(password) / sizeof(char));
+        } else if (userChoice == 3) {
+            printf("Welcome to the prime checker.\n");
+            printf("Enter a number to check if it is a prime number: ");
+            int number;
+            scanf("%d", &number);
+            primeChecker(number);
+        } else if (userChoice == 4) {
+            printf("Bye for now!\n");
+            return 0;
+        } else {
+            printf("\nInvalid input, please enter a menu option.\n(press enter to continue)\n");
+            while (getchar() != '\n');
+        }
+    }
+}
